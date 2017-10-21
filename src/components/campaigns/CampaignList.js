eee262ef5a12e461b4be543847fa4794885722a5
Antonio <antonio.hdez93@gmail.com>
2017-10-21 03:31:37 -0500

    Campaign status icon in campaign item

--- src/components/campaigns/CampaignList.js
+++ src/components/campaigns/CampaignList.js
@@ -10,7 +10,12 @@ class Header extends Component {
                 </div>
                 <div className="campaign-item">
                     <div className="campaign-item__status-bar">
-                        <span className="campaign-item__status-bar-item">Activo</span>
+                        <span className="campaign-item__status-bar-item">
+                            <span className="campaign-status">
+                                <span className="fa fa-fw fa-check-circle campaign-status__icon campaign-status__icon--ok"></span>
+                                <span className="campaign-status__text">Activo</span>
+                            </span>
+                        </span>
                         <span className="campaign-item__status-bar-item campaign-header-menu">
                             <i className="fa fa-fw fa-ellipsis-h"></i>
                             <ul className="sub-menu sub-menu--left-of-parent">
@@ -40,7 +45,12 @@ class Header extends Component {
                 </div>
                 <div className="campaign-item">
                     <div className="campaign-item__status-bar">
-                        <span className="campaign-item__status-bar-item">Activo</span>
+                        <span className="campaign-item__status-bar-item">
+                            <span className="campaign-status">
+                                <span className="fa fa-fw fa-warning campaign-status__icon campaign-status__icon--warning"></span>
+                                <span className="campaign-status__text">Por expirar</span>
+                            </span>
+                        </span>
                         <span className="campaign-item__status-bar-item campaign-header-menu">
                             <i className="fa fa-fw fa-ellipsis-h"></i>
                             <ul className="sub-menu sub-menu--left-of-parent">
@@ -66,6 +76,41 @@ class Header extends Component {
                     <div className="campaign-item__details">
                         <div className="campaign-item__details-title">Reporte Ciudadano</div>
                         <div className="campaign-item__details-hashtag">#aguas</div>
+                    </div>
+                </div>
+                <div className="campaign-item">
+                    <div className="campaign-item__status-bar">
+                        <span className="campaign-item__status-bar-item">
+                            <span className="campaign-status">
+                                <span className="fa fa-fw fa-times-circle campaign-status__icon campaign-status__icon--expired"></span>
+                                <span className="campaign-status__text">Inactivo</span>
+                            </span>
+                        </span>
+                        <span className="campaign-item__status-bar-item campaign-header-menu">
+                            <i className="fa fa-fw fa-ellipsis-h"></i>
+                            <ul className="sub-menu sub-menu--left-of-parent">
+                                <li className="sub-menu__item">
+                                    <i className="fa fa-fw fa-info"></i>
+                                    <span> Ver detalle</span>
+                                </li>
+                                <li className="sub-menu__item">
+                                    <i className="fa fa-fw fa-pencil"></i>
+                                    <span> Editar</span>
+                                </li>
+                                <div className="sub-menu__divider"></div>
+                                <li className="sub-menu__item">
+                                    <i className="fa fa-fw fa-times"></i>
+                                    <span> Eliminar</span>
+                                </li>
+                            </ul>
+                        </span>
+                    </div>
+                    <div className="campaign-item__image">
+                        <img src="oracle.png" alt="campaign image" />
+                    </div>
+                    <div className="campaign-item__details">
+                        <div className="campaign-item__details-title">Oracle MDC</div>
+                        <div className="campaign-item__details-hashtag">#OracleMDC</div>
                     </div>
                 </div>
             </div>
